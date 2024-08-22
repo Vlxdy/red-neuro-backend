a10a1207a398d93b6d026c66879e66171134a687
Neil Graneros Flores <neilgraneros11@gmail.com>
2024-08-22 11:23:39 -0400

    fix: :package: se reemplazó el paquete ip por ip-address

--- src/core/logger/utilities/get-ip-address.ts
+++ src/core/logger/utilities/get-ip-address.ts
@@ -1,5 +1,23 @@
-import ip from 'ip'
+import { networkInterfaces } from 'os'
+import { Address4, Address6 } from 'ip-address'
 
 export function getIPAddress(): string {
-  return String(ip.address())
+  const nets = networkInterfaces()
+
+  for (const netName of Object.keys(nets)) {
+    const netInfo = nets[netName]
+    if (!netInfo) continue
+
+    for (const net of netInfo) {
+      if (!net.internal) {
+        if (net.family === 'IPv4' && Address4.isValid(net.address)) {
+          return new Address4(net.address).correctForm()
+        } else if (net.family === 'IPv6' && Address6.isValid(net.address)) {
+          return new Address6(net.address).correctForm()
+        }
+      }
+    }
+  }
+
+  throw new Error('No se encontró una dirección IP externa válida')
 }
